eb58e7ccdf4cf91a7e4bc1c2314e9e67c975619a
johnmuthua <caveomuthama@gmail.com>
2023-01-30 14:08:00 +0300

    added butler function

---
+++ basic/butler.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void butler(void);
+int main(void) {
+	printf("Summon the butler function \n");
+	butler();
+	printf("Get me some coffe \n");
+}
+
+void butler(void){
+	printf("You caller sir? \n");
+}
